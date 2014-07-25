12403c8850b8d72b8e8e834827534286574a8c01
Mike Land <mikepland@gmail.com>
2014-07-25 03:03:23 -0500

    sets up rankings and pulls data from local host

--- javascript/leaderboard.js
+++ javascript/leaderboard.js
@@ -1,13 +1,15 @@
 function getLeaderboard($scope, $http)
 {
-  $http({method: 'GET', url: 'http://lando2319-craps_buzz_leaderboard.nodejitsu.com/api/users'}).
+  $http({method: 'GET', url: 'http://localhost:3000/api/users'}).
+  //$http({method: 'GET', url: 'http://lando2319-craps_buzz_leaderboard.nodejitsu.com/api/users'}).
     success(function(data, status, headers, config) {
       // this callback will be called asynchronously
       // when the response is available
 //      console.log(data)
-      angular.forEach(data, function(value) {
+      angular.forEach(data, function(value, key) {
        // console.log(value)
-        $scope.leaders.push({name: value.name, cashout_amount: value.cashout_amount})
+        console.log(key)
+        $scope.leaders.push({rank: key + 1, name: value.name, cashout_amount: value.cashout_amount})
       })
 
     }).
