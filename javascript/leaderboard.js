72b06328f7838c24e7d30a68ea35f3f9a0d87890
Mike Land <mikepland@gmail.com>
2014-07-25 05:52:30 -0500

    sets up leaderboard notice

--- javascript/leaderboard.js
+++ javascript/leaderboard.js
@@ -7,10 +7,13 @@ function runLeaderboard($scope) {
     }
   })
 
+  var distance_to_first = "Earn " + ($scope.leaders[0].cashout_amount - $scope.bank_roll_actual + 1) + " more coins and you'll be able to rank Number 1"
+
   if ($scope.available_leaderboard_position > 0 ) {
-    $scope.available_leaderboard_position_message = "Cashout now to grab the number " + $scope.available_leaderboard_position + " slot on the leaderboard"
+    $scope.available_leaderboard_position_message = "Cashout now to grab the number " + $scope.available_leaderboard_position + " slot on the leaderboard. " + distance_to_first
+    $scope.player_game_calls.push({call_actual: "Cashout now to rank " + $scope.available_leaderboard_position})
   } else {
-    $scope.available_leaderboard_position_message = "Earn " + ($scope.leaders[9].cashout_amount - $scope.bank_roll_actual + 1) + " more coins to grab a slot on the leaderboard"
+    $scope.available_leaderboard_position_message = "Earn " + ($scope.leaders[9].cashout_amount - $scope.bank_roll_actual + 1) + " more coins to grab a slot on the leaderboard. " + distance_to_first
   }
 }
 
